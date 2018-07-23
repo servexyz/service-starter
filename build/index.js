054c7274a0b8d35b87441c361c615c0a23abe7bd
alechp <me@alechp.com>
2018-07-22 19:22:48 -0700

    Added nvmrc to fix ava engine incompatibility.
    * Removed pkg lock in favor of yarn lock

--- build/index.js
+++ build/index.js
@@ -7,11 +7,19 @@ const {
   get
 } = require("microrouter");
 
+const root = (req, res) => send(res, 200, "Running");
+
 const hello = (req, res) => send(res, 200, `Hello ${req.params.who}`);
 
 const notfound = (req, res) => send(res, 404, "No route found");
 
-module.exports = router(get("/hello/:who", hello), get("/*", notfound));
+module.exports = router(get("/", root), get("/hello/:who", hello), get("/*", notfound));
+/* 
+  ! NOTE: "/*" is not an exact match. 
+  ! Meaning, notfound path must be last. 
+  ! If first, it will prevent all other routes from being identified
+*/
+
 /* eg. 
 Dev: http://localhost:3000/hello/alec
 Dev:built: http://localhost:3000/hello/alec
